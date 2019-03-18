fb8205f575d9c3032fe8e8d48c484effcf9d5972
Stilet850 <stilet850@mail.ru>
2019-03-18 18:22:00 +0300

    First task.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,38 @@
+import java.util.*;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static final int DEFAULT_LENGTH = 10000;
+    private Resume[] storage = new Resume[DEFAULT_LENGTH];
+    private int lastId = 0;
 
     void clear() {
+		Arrays.fill(this.storage, null);
     }
 
     void save(Resume r) {
+    this.storage[lastId]= r;
+    lastId++;
     }
 
     Resume get(String uuid) {
-        return null;
+          return Arrays.stream(this.storage).filter(Objects::nonNull).filter(resume -> resume.uuid.equals(uuid)).findFirst().orElse(new Resume());
     }
 
     void delete(String uuid) {
+        this.storage = Arrays.stream(this.storage).filter(Objects::nonNull).filter(resume -> !(resume.uuid.equals(uuid))).toArray(Resume[]::new);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.stream(this.storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     int size() {
-        return 0;
+       return this.storage.length;
     }
 }
