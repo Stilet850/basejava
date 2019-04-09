cd39040668e4bcb9175b8b0a460d37b5adc6fc45
Slava Rakhmvi <stilet850@mail.ru>
2019-04-09 12:12:45 +0300

    use methods from Arrays.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,8 +2,8 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-import java.util.Objects;
+import static java.util.Arrays.copyOfRange;
+import static java.util.Arrays.fill;
 
 /**
  * Array based storage for Resumes
@@ -14,62 +14,58 @@ public class ArrayStorage {
     private int size;
 
     public void clear() {
-        for (int i = 0; i < size; i++)
-            storage[i] = null;
+        fill(storage, 0, size, null);
 
         size = 0;
     }
 
-    public void update(Resume r){
-        if(getIndex(r.getUuid())==-1) {
+    public void update(Resume r) {
+        if (getIndex(r.getUuid()) == -1) {
             System.err.println("Resume doesn't exist, uuid : " + r.getUuid());
-        }else{
+        } else {
             storage[size] = r;
         }
     }
 
     public void save(Resume r) {
         if (r.getUuid() == null || r.getUuid().isEmpty())
-           System.err.println("Invalid UUID " + r.getUuid());
+            System.err.println("Invalid UUID " + r.getUuid());
 
         if (size == DEFAULT_LENGTH)
             System.err.println("Storage is full");
 
-        if(getIndex(r.getUuid())!=-1) {
+        if (getIndex(r.getUuid()) != -1) {
             System.err.println("Resume already exists, uuid : " + r.getUuid());
-        }else{
+        } else {
             storage[size] = r;
             size++;
         }
     }
 
     public Resume get(String uuid) {
-        if(getIndex(uuid) == -1){
+        if (getIndex(uuid) == -1) {
             System.err.println("There is no Resume in storage, uuid : " + uuid);
             return null;
-        }else{
+        } else {
             return storage[getIndex(uuid)];
         }
     }
 
     public void delete(String uuid) {
-         if (getIndex(uuid) == -1){
-             System.err.println("There is no Resume in storage, uuid : " + uuid);
-         }else{
-             storage[getIndex(uuid)] = storage[size - 1];
-             storage[size - 1] = null;
-             size--;
-         }
+        if (getIndex(uuid) == -1) {
+            System.err.println("There is no Resume in storage, uuid : " + uuid);
+        } else {
+            storage[getIndex(uuid)] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
+
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume [] resume = new Resume[size];
-        for (int i = 0; i < size ; i++) {
-            resume[i] = storage[i];
-        }
-        return resume;
+        return copyOfRange(storage, 0, size);
     }
 
     /**
@@ -85,11 +81,11 @@ public class ArrayStorage {
      * @param uuid
      * @return
      */
-    private int getIndex(String uuid){
-            for (int i = 0; i < size; i++)
-                if (uuid.equals(storage[i].getUuid()))
-                  return i;
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++)
+            if (uuid.equals(storage[i].getUuid()))
+                return i;
 
-                return -1;
+        return -1;
     }
 }
